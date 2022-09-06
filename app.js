ac3d23a1227e499950c8bb75e5f217790d98069a
yuvraj1905 <yuvrajkumar2509@gmail.com>
2022-09-06 12:19:04 +0530

    done coding app

--- app.js
+++ app.js
@@ -2,7 +2,24 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+var serverUrl = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationUrl(input) {
+  return serverUrl + "?" + "text=" + input;
+}
+function errorHandler(error) {
+  console.log("error occured", error);
+  alert("Something wrong with server , Please try after sometime.");
+}
+
 function clickHandler() {
-  outputDiv.innerText = "thisHolaa " + txtInput.value;
+  var inputText = txtInput.value;
+  fetch(getTranslationUrl(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler);
 }
 btnTranslate.addEventListener("click", clickHandler);
